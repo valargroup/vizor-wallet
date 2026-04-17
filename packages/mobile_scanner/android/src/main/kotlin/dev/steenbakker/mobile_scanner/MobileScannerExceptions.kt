e532843638fc6a1b994ffe0295d7eec718172580
Thunnini <yunjh1994@gmail.com>
2026-04-17 17:12:50 +0900

    Use git mobile_scanner fork instead of vendored copy

--- packages/mobile_scanner/android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScannerExceptions.kt
+++
@@ -1,9 +0,0 @@
-package dev.steenbakker.mobile_scanner
-
-class NoCamera : Exception()
-class AlreadyStarted : Exception()
-class AlreadyStopped : Exception()
-class AlreadyPaused : Exception()
-class CameraError : Exception()
-class ZoomWhenStopped : Exception()
-class ZoomNotInRange : Exception()
